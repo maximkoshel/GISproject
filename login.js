2bdf3f9afc3f36f1fcc8f6ed4ce736e207fda037
maximkoshel <maximkoshel95@gmail.com>
2021-05-23 11:17:27 +0300

    main files

---
+++ login.js
@@ -0,0 +1,92 @@
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+var app = express();
+
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'nodelogin'
+});
+
+
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+app.use(express.static(__dirname+'/public'));
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+app.get('/register', function(request, response) {
+	response.sendFile(path.join(__dirname + '/register.html'));
+});
+
+app.get('/login', function(request, response) {
+	response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+//Need to create home page
+app.get('/home', function(request, response) {
+	if (request.session.loggedin) {
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+app.post('/auth', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+app.post('/register', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	var email = request.body.email;
+	if(request.body.gymOwner){
+	var gywOwner = 'YES';}
+	else
+	{var gywOwner = 'NO';}
+
+	if (username && password && email) {
+		connection.query('INSERT INTO accounts (username, password, email, IsGymOwner) VALUES (?, ?, ?,?)', [username, password,email,gywOwner], function(error, results, fields) {
+			response.send('You are registered!');
+			response.end();
+		});
+	} else {
+		response.send('Something Empty!');
+		response.end();
+	}
+});
+
+
+
+app.listen(3000);
